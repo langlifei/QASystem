4b0b44a6d7c5f76aec373d609d5f17cd80fff720
langlifei <1084327512@qq.com>
2020-04-20 14:54:45 +0800

    add update the function of login in the LoginController

--- src/main/java/com/test/entities/WorkRecord.java
+++ src/main/java/com/test/entities/WorkRecord.java
@@ -1,5 +1,7 @@
 package com.test.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -10,8 +12,10 @@ public class WorkRecord implements Serializable {
 
     private String question;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date beginDate;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date endDate;
 
     private String comment;
